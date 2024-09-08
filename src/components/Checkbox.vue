9c19babbf855b4aead6148e9cfc73104d786e9a5
Ray Guo <33137074+RayGuo-ergou@users.noreply.github.com>
2024-09-08 19:57:07 +1000

    batman

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,27 @@
+<template>
+  <CheckboxRoot
+    v-bind="forwarded"
+    v-model:checked="checked"
+  >
+    <CheckboxIndicator class="flex size-full items-center justify-center text-current">
+      <slot>
+        <IRmvInterfaceCheck class="size-4" />
+      </slot>
+    </CheckboxIndicator>
+  </CheckboxRoot>
+</template>
+
+<script setup lang="ts">
+import { CheckboxIndicator, CheckboxRoot, useForwardPropsEmits } from 'radix-vue';
+import { CheckboxProps, CheckboxEmits } from './type.ts'
+
+const props = defineProps<CheckboxProps>();
+const emits = defineEmits<CheckboxEmits>();
+
+
+// Both v-model="" and v-model:checked="" will work
+const checked = defineModel<boolean>();
+
+const forwarded = useForwardPropsEmits(props, emits);
+</script>
+
---
+++ src/components/type.ts
@@ -0,0 +1,7 @@
+import type { CheckboxRootEmits, CheckboxRootProps } from 'radix-vue';
+
+export interface CheckboxProps extends CheckboxRootProps {
+}
+export interface CheckboxEmits extends CheckboxRootEmits {
+}
+
